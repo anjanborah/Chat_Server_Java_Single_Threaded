7831e504e17f2c94b9117974d4efd66f9c99e114
Anjan Borah <anjan.borah88@yahoo.in>
2013-05-01 19:41:21 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,85 @@
+/*
+* A simple chat server
+* GitHub - https://github.com/anjanborah/Chat_Server_Java_Single_Threaded
+* Author - Anjan Borah
+* Copyright ( c ) 2013 Anjan Borah
+*/
+
+import java.util.Scanner;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+public class Main {
+  public static void main( String args[] ) {
+    Chat_Server object = new Chat_Server();
+  }
+}
+
+class Chat_Server {
+
+  public Chat_Server() {
+    try {
+      this.start_server();
+      this.engine();
+      this.stop_server();
+    } catch( Exception exception ) {
+      System.out.println( "Exception - " + exception.toString() );
+      System.exit( 0 );
+    }
+  }
+
+  private void start_server() {
+    try {
+    this.chat_server_parent = new ServerSocket( 8888 );
+    this.chat_server_child = this.chat_server_parent.accept();
+    this.input_stream = this.chat_server_child.getInputStream();
+    this.output_stream = this.chat_server_child.getOutputStream();
+    this.from_client = new Scanner( this.input_stream );
+    this.to_client = new PrintWriter( this.output_stream, true );
+    } catch( Exception exception ) {
+      System.out.println( "Exception - " + exception.toString() );
+    }
+  }
+  
+  private void engine() {
+    try {
+      this.to_client.println( "+----------------------------------------------------------------------+" );
+      this.to_client.println( "|                             CHAT SERVER                              |" );
+      this.to_client.println( "|    Type bye to exit. Wait for the reply after sending a message      |" );
+      this.to_client.println( "+----------------------------------------------------------------------+\n\n" );
+      while( this.from_client.hasNextLine() ) {
+        String message = this.from_client.nextLine();
+        if( message.trim().equals( "bye" ) || message.trim().equals( "Bye" ) ) {
+          this.stop_server();
+        } else {
+          System.out.println( "\n" + this.chat_server_child.getRemoteSocketAddress().toString() + " - " + message );
+          System.out.print( "Reply - " );
+          String reply = this.get.nextLine();
+          this.to_client.println( "\n" + reply );
+        }
+      }
+    } catch( Exception exception ) {
+      System.out.println( "Exception - " + exception.toString() );
+    }
+  }
+  
+  private void stop_server() {
+    try {
+      this.chat_server_parent.close();
+      this.chat_server_child.close();
+    } catch( Exception exception ) {
+      System.out.println( "Exception - " + exception );
+    }
+  }
+  
+  private Scanner get = new Scanner( System.in );
+  private ServerSocket chat_server_parent;
+  private Socket chat_server_child;
+  private InputStream input_stream;
+  private OutputStream output_stream;
+  private Scanner from_client;
+  private PrintWriter to_client;
+}
